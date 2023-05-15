807136f2f6c731a3b6b064ea45e716699c8dc883
JayceAronGo <jaycearongo@plv.edu.ph>
2023-05-15 17:14:10 +0800

    vmmnm

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import OurWorks from "./components/OurWorks";
 import  Packages  from "./components/Packages";
 import PackageDetail from "./components/PackagesDetails";
 import Calendar from "./pages/Calendar";
+import ProjectDetails from "./pages/ProjectDetails";
 
 function App() {
   return (
 <div >
 <Navbar />
-<Calendar/>
+<ProjectDetails/>
 <Footer />
 </div>
   )
---
+++ src/pages/ProjectDetails.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import Vapoly from "../assets/vapoly.jpg";
+import Valogo from  "../assets/valogo.png";
+import {AiFillFacebook} from "react-icons/ai";
+import {AiFillYoutube} from "react-icons/ai";
+import {AiFillInstagram} from "react-icons/ai";
+
+
+const ProjectDetails = () => {
+  return (
+    <div>
+        <img className="w-full h-full object-cover " src={Vapoly} alt="bgm"></img>
+
+   
+    <div className="flex items-center justify-center">
+
+        <div className="absolute border-none z-14 top-1/4 right-1/4 rounded-xl p-4 w-1/2 h-fix  pb-40 bg-black bg-opacity-75 ">
+          <div className=" py-60">
+              <div className="">
+
+              {/* Project Backend */}
+              <div>
+
+              </div>
+              
+              </div>
+          
+          </div>
+        </div>
+
+        <div className=" absolute z-16 top-64 right-1/2 ">
+          <div className=''>
+            <h1 className='text-white text-5xl text-end '> Rules and Guidelines </h1>
+            <ul>
+              <li>
+
+              </li>
+            </ul>
+          </div>
+        </div>
+
+
+
+        <div className='flex absolute z-16 top-3/4 left-1/2'>
+          <div className=' flex pr-3'>
+          <div className=" py-20 ">
+            <div className=" cursor-pointer text-center font-bold  text-2xl py-1 px-16 border-2 border-solid  rounded-3xl text-sky-600 border-sky-600 hover:scale-105 duration-300">
+              <h1>Go Back</h1>
+            </div>
+          </div>
+
+          <div className=" py-20 px-4  ">
+            <div className=" cursor-pointer text-center font-bold  text-2xl py-1 px-16 border-4 border-none  rounded-3xl text-white bg-sky-600 hover:scale-105 duration-300">
+              <h1>Continue</h1>
+            </div>
+          </div>
+          </div>
+        </div>
+
+    </div>
+
+ <section className="bg-gray-300 py-10 w-full" >
+      <div className="container mx-auto">
+      </div>
+    </section>
+    </div>
+  )
+}
+export default ProjectDetails
